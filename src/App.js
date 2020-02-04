e328e2494b949b08cf8a8ed122818ca1d92ae86d
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-02-04 22:55:23 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import { BrowserRouter, Router, Link } from 'react-router-dom';
-import Test from '.Test.js';
-import Test2 from '.Test2.js';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Test from './Test.js';
+import Test2 from './Test2.js';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="navigation">
-        <img src={logo} className="logo" alt="logo Image" />
-        <div className="navigation-sub">
-          <a href="" className="item">Test</a>
-          <a href="" className="item">Test 2</a>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Test} />
+        <Route path="/Test2" component={Test2} />
+
+        <div className="navigation">
+          <img src={logo} className="logo" alt="logo Image" />
+          <div className="navigation-sub">
+            <a href="/" className="item">Test</a>
+            <a href="/Test2" className="item">Test 2</a>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
