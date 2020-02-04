ae7fccafae5149d423c1cdb6b9b0d8717f0824cd
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-02-04 23:08:28 +0100

    thumbnail

--- src/Test.js
+++ src/Test.js
@@ -1,15 +1,30 @@
 import React from "react"
- 
+import Thumbnail from './Thumbnails.js'
 function Test(props) {
   return (
-    <div className="project">
-      <a href="./experiments">
-        <div className="project-image">
-          <img src="./experiments-image" alt="Project Image"/>
-        </div>
-        <div className="project-title">Experiments</div>
-        <div className="project-category">Personal</div>
-      </a>
+    <div>
+      <h1>Test</h1>
+      <Thumbnail
+        link="/twitter"
+        image="http://twitter-image-url.jpg"
+        title="Twitter Newsfeed"
+        category="Mobile App"
+      />
+      
+      <Thumbnail
+        link="/airbnb"
+        image="http://airbnb-image-url.jpg>"
+        title="Airbnb Experiences"
+        category="Website"
+      />
+      
+      <Thumbnail
+        link="/photoshop"
+        image="http://photoshop-image-url.jpg"
+        title="Photoshop Redesign"
+        category="Desktop App"
+      />
+
     </div>
   )
 }
