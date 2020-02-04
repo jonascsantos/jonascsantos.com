1218132b0eed202991dc045e5a9989d328f09c5d
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-02-04 22:57:48 +0100

    thumbnail

--- src/Test.js
+++ src/Test.js
@@ -2,8 +2,14 @@ import React from "react"
  
 function Test(props) {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="project">
+      <a href="./experiments">
+        <div className="project-image">
+          <img src="./experiments-image" alt="Project Image"/>
+        </div>
+        <div className="project-title">Experiments</div>
+        <div className="project-category">Personal</div>
+      </a>
     </div>
   )
 }
