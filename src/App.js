a0eec870b9c47ea7936e327611fd174d253991bb
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-04-14 22:51:58 +0200

    Typical library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hi, I'm Jonas dos Santos</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a {' '}
+          <Typical
+            loop={Infinity}
+            wrapper="b"
+            steps={[
+              'developer ',
+              1500,
+              'Brazilian student',
+              1500,
+              'Open Sourcer',
+              1500,
+              "designer (like a 6.2/10 xD)",
+              1500,
+              "dreamer",
+              2000,
+
+
+            ]}
+          />
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
