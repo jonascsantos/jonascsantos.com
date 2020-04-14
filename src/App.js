6d5b6f833c69beed3561db7066eed743a290c9d2
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-04-14 20:27:11 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Test from './Test.js';
-import Test2 from './Test2.js';
 import './App.css';
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Route exact path="/" component={Test} />
-        <Route path="/Test2" component={Test2} />
-
-        <div className="navigation">
-          <img src={logo} className="logo" alt="logo Image" />
-          <div className="navigation-sub">
-            <a href="/" className="item">Test</a>
-            <a href="/Test2" className="item">Test 2</a>
-          </div>
-        </div>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
