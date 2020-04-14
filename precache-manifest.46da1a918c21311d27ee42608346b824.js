df4df72d172276dd278965e0e32cdbfeebf99b01
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-04-14 20:31:47 +0200

    Updates

---
+++ precache-manifest.46da1a918c21311d27ee42608346b824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbd72f29d8ed362b9e2d9d74f074d9b",
+    "url": "/jonascsantos.com/index.html"
+  },
+  {
+    "revision": "6ad74c1ba871613d2dbe",
+    "url": "/jonascsantos.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d22967e0788e5268a39",
+    "url": "/jonascsantos.com/static/js/2.1e30868a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jonascsantos.com/static/js/2.1e30868a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad74c1ba871613d2dbe",
+    "url": "/jonascsantos.com/static/js/main.ff909b92.chunk.js"
+  },
+  {
+    "revision": "fb92b6628058e8141dda",
+    "url": "/jonascsantos.com/static/js/runtime-main.21ee42f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jonascsantos.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
