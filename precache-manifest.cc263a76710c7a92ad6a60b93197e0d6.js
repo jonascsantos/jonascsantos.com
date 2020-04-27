aa478948467f1458425033cbbc9e96e697bbe06a
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-04-27 19:36:14 +0200

    Updates

---
+++ precache-manifest.cc263a76710c7a92ad6a60b93197e0d6.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f704ed79c4615ce79f6f7b64d5f92669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279fde936ab517b175f1",
+    "url": "/static/css/main.1a6ba6bb.chunk.css"
+  },
+  {
+    "revision": "19e72e3d93932ba8e882",
+    "url": "/static/js/2.3b444f35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b444f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279fde936ab517b175f1",
+    "url": "/static/js/main.fcc55b39.chunk.js"
+  },
+  {
+    "revision": "7ac7e24afc80e48d78bc",
+    "url": "/static/js/runtime-main.92efb0e6.js"
+  },
+  {
+    "revision": "183de736efe789394f26ed013b0b5cdd",
+    "url": "/static/media/Calibre-Light.183de736.woff2"
+  },
+  {
+    "revision": "62b8f0fd78f4d842b3a75c1d2512e449",
+    "url": "/static/media/Calibre-Light.62b8f0fd.woff"
+  },
+  {
+    "revision": "73b7f4385ae2f8028e1b365a48038582",
+    "url": "/static/media/Calibre-Light.73b7f438.ttf"
+  },
+  {
+    "revision": "1222cf0d45062719e06c3d7d44937e8f",
+    "url": "/static/media/Calibre-LightItalic.1222cf0d.woff"
+  },
+  {
+    "revision": "3ede1be73648125ed40cfd07e1292bf1",
+    "url": "/static/media/Calibre-LightItalic.3ede1be7.ttf"
+  },
+  {
+    "revision": "f34c1430353885b55d271d473235eb6a",
+    "url": "/static/media/Calibre-LightItalic.f34c1430.woff2"
+  },
+  {
+    "revision": "115d37180a9c07e2f480ca81b032babe",
+    "url": "/static/media/Calibre-Medium.115d3718.woff"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "/static/media/Calibre-Medium.46b36969.woff2"
+  },
+  {
+    "revision": "88178c3f2a309ad11520ee62765072d0",
+    "url": "/static/media/Calibre-Medium.88178c3f.ttf"
+  },
+  {
+    "revision": "3df14639dd2cb07c83bd39707d70910f",
+    "url": "/static/media/Calibre-MediumItalic.3df14639.woff2"
+  },
+  {
+    "revision": "4afe8032c647b57de924543c052e0ee4",
+    "url": "/static/media/Calibre-MediumItalic.4afe8032.woff"
+  },
+  {
+    "revision": "6eb2ac5a1d38f19c7085b131356b355e",
+    "url": "/static/media/Calibre-MediumItalic.6eb2ac5a.ttf"
+  },
+  {
+    "revision": "96e0a7c47fdd7a8f05007837ead73b35",
+    "url": "/static/media/Calibre-Regular.96e0a7c4.ttf"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "/static/media/Calibre-Regular.b63c62e5.woff2"
+  },
+  {
+    "revision": "e08232148510aade507c2af9bb66acd0",
+    "url": "/static/media/Calibre-Regular.e0823214.woff"
+  },
+  {
+    "revision": "35657aa2d12ff78e3d8a8a5ba28c2c35",
+    "url": "/static/media/Calibre-RegularItalic.35657aa2.woff2"
+  },
+  {
+    "revision": "3a445970c7bcd6b102ac6bd60178b0bc",
+    "url": "/static/media/Calibre-RegularItalic.3a445970.woff"
+  },
+  {
+    "revision": "c408807a3001d1f7773f5f7fe513c165",
+    "url": "/static/media/Calibre-RegularItalic.c408807a.ttf"
+  },
+  {
+    "revision": "347808401d7a6607ff9a86897aec0a39",
+    "url": "/static/media/Calibre-Semibold.34780840.woff"
+  },
+  {
+    "revision": "8325d174436d55e995c7214faafcd47b",
+    "url": "/static/media/Calibre-Semibold.8325d174.ttf"
+  },
+  {
+    "revision": "d8b856473a51d7eec2a9bccf2fbea54c",
+    "url": "/static/media/Calibre-Semibold.d8b85647.woff2"
+  },
+  {
+    "revision": "6473be72d5216d0ec21d273af09b9fa3",
+    "url": "/static/media/Calibre-SemiboldItalic.6473be72.woff"
+  },
+  {
+    "revision": "71941c0c0420e703494d6e2a62bc12d4",
+    "url": "/static/media/Calibre-SemiboldItalic.71941c0c.ttf"
+  },
+  {
+    "revision": "8f3e4b3eaed73e67bbc4fc81f308a35f",
+    "url": "/static/media/Calibre-SemiboldItalic.8f3e4b3e.woff2"
+  },
+  {
+    "revision": "02bbb63875ebdbf463e764bb3d8ff190",
+    "url": "/static/media/SFMono-Medium.02bbb638.ttf"
+  },
+  {
+    "revision": "7df7729fbc66c426b3500d72bc140f5c",
+    "url": "/static/media/SFMono-Medium.7df7729f.woff"
+  },
+  {
+    "revision": "99497ab65b5b8a7e5c18b7b3edde5c85",
+    "url": "/static/media/SFMono-Medium.99497ab6.woff2"
+  },
+  {
+    "revision": "92f9fb02060aabf378f0639de5e033ef",
+    "url": "/static/media/SFMono-MediumItalic.92f9fb02.ttf"
+  },
+  {
+    "revision": "9fbdbe4e2d56f80d6775b61f9b264df8",
+    "url": "/static/media/SFMono-MediumItalic.9fbdbe4e.woff"
+  },
+  {
+    "revision": "ae7ea80fb472a7d27aa69b3cf00f487c",
+    "url": "/static/media/SFMono-MediumItalic.ae7ea80f.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "/static/media/SFMono-Regular.7faa6660.woff2"
+  },
+  {
+    "revision": "d59c1702c527525e9939ba06166a3750",
+    "url": "/static/media/SFMono-Regular.d59c1702.woff"
+  },
+  {
+    "revision": "d892da9f8895428033ec68a6c0426ba6",
+    "url": "/static/media/SFMono-Regular.d892da9f.ttf"
+  },
+  {
+    "revision": "502441ff6ee56a1df1558e8461050a46",
+    "url": "/static/media/SFMono-RegularItalic.502441ff.woff"
+  },
+  {
+    "revision": "8eddf7adea62d66b9e4d031b58dfaaa6",
+    "url": "/static/media/SFMono-RegularItalic.8eddf7ad.ttf"
+  },
+  {
+    "revision": "bb1fc3ab1772befac3587b30ca876a09",
+    "url": "/static/media/SFMono-RegularItalic.bb1fc3ab.woff2"
+  },
+  {
+    "revision": "2d4bff46a7a9ba9dc7701f75726a7815",
+    "url": "/static/media/SFMono-Semibold.2d4bff46.woff"
+  },
+  {
+    "revision": "8282ed99a84b8e9c803baf3df5b76c01",
+    "url": "/static/media/SFMono-Semibold.8282ed99.ttf"
+  },
+  {
+    "revision": "cebe2bfbe8a283427e3c5ad8a1b8ae2f",
+    "url": "/static/media/SFMono-Semibold.cebe2bfb.woff2"
+  },
+  {
+    "revision": "2fd68bed64aeaceadc6402efc4d82767",
+    "url": "/static/media/SFMono-SemiboldItalic.2fd68bed.woff2"
+  },
+  {
+    "revision": "41da0311eaa7077546785a822a6ba21a",
+    "url": "/static/media/SFMono-SemiboldItalic.41da0311.woff"
+  },
+  {
+    "revision": "ca70ba8a9030ed9bb7e081f63fd3831b",
+    "url": "/static/media/SFMono-SemiboldItalic.ca70ba8a.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7112f8c120dd997712b3765cbb6401ea.js"
+  "/precache-manifest.cc263a76710c7a92ad6a60b93197e0d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
